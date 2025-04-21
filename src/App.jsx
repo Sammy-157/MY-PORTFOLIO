020107bc11ae5a0cb055e93d4311b04861217d8b
Sammy-157 <samuelamankwah157@gmail.com>
2025-04-21 01:41:32 +0000

    Fix import casing for Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar"; 
+import Navbar from "./Components/Navbar"; 
 import Home from "./pages/Home";
 import  Services from "./pages/Services";
 import About from "./pages/About";
