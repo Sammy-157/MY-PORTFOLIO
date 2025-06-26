86383edf049c77e7fe838f647bdff94b4736c17e
Sammy-157 <samuelamankwah157@gmail.com>
2025-06-26 18:55:46 +0000

    portfolio enhancement

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,59 +1,75 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../Styles/Contact.css";
-import  contactImage from "../assets/contact.jpg";
+import contactImage from "../assets/contact.jpg";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { FaEnvelope, FaPhoneAlt, FaMapMarkerAlt } from "react-icons/fa";
 
 const Contact = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
   return (
     <div className="main-div" id="contact">
-      <h1>FEEL FREE TO CONTACT ME!</h1>
-      <div className="contact-sections">
-        <div className="image-div">
-          <img src={contactImage}  alt="contact pic" className="image"></img>
-        </div>
-        <div className="contact-1">
-          
+      <h1 data-aos="fade-down">FEEL FREE TO CONTACT ME!</h1>
 
+      <div className="contact-sections">
+        <div className="image-div" data-aos="zoom-in">
+          <img src={contactImage} alt="contact pic" className="image" />
+        </div>
+
+        <div className="contact-1" data-aos="fade-up">
           <div className="details-main">
             <div className="details-1">
               <div className="fullName">
-                <label htmlFor="First Name"> Full Name</label>
+                <label htmlFor="fullName">Full Name</label>
                 <input
                   type="text"
-                  id="First Name"
+                  id="fullName"
+                  placeholder="e.g. Samuel Amankwah"
                   required
                   className="box"
-                ></input>
+                />
               </div>
 
               <div className="email">
-                <label htmlFor="email"> E-mail</label>
-                <input type="email" id="email" required className="box"></input>
+                <label htmlFor="email">E-mail</label>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="e.g. samuel@example.com"
+                  required
+                  className="box"
+                />
               </div>
 
               <div className="message">
-                <label htmlFor="Message"> Phone Number</label>
+                <label htmlFor="phone">Phone Number</label>
                 <input
-                  type="Phone"
-                  id="Phone"
+                  type="tel"
+                  id="phone"
+                  placeholder="e.g. +233 555 000 000"
                   required
                   className="box"
-                ></input>
+                />
               </div>
+
               <div className="button-wrapper">
-                <button>Contact me</button>
+                <button className="contact-button">Contact Me</button>
               </div>
             </div>
 
-            <div className="details-2">
+            <div className="details-2" data-aos="fade-left">
               <div className="contact-2">
-                <b>Contact</b>
+                <b><FaEnvelope /> Contact</b>
                 <a href="mailto:samuelamakwah157@gmail.com">
                   samuelamakwah157@gmail.com
                 </a>
               </div>
               <div className="location">
-                <b>Based in</b>
-                Accra, Takoradi,<br></br> Ghana
+                <b><FaMapMarkerAlt /> Based in</b>
+                Accra, Takoradi,<br />Ghana
               </div>
             </div>
           </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,32 +1,54 @@
 import React from "react";
 import "../Styles/Home.css";
-import sammy from  "../assets/sammy.jpg";
+import sammy from "../assets/sammy.jpg";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <div className="home-main" id="home">
-      <h1>HOMEPAGE</h1>
+    <motion.div
+      className="home-main"
+      id="home"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
       <div className="home-sections">
-        <div className="home-left">
+        {/* Left Section */}
+        <motion.div
+          className="home-left"
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
           <b>HELLO,</b>
           <p>I AM SAMUEL KOFI NTEM AMANKWAH,</p>
-          <p> A JUNIOR SOFTWARE DEVELOPER</p>
+          <p>A JUNIOR SOFTWARE DEVELOPER</p>
           <b>WELCOME</b>
           <p>THIS IS MY PORTFOLIO</p>
+
           <div className="resume">
             <b>DOWNLOAD RESUME HERE 👇</b>
-            
-            <button className="button-wrapper">DOWNLOAD</button>
-            
+            <motion.button
+              className="button-wrapper"
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
+            >
+              DOWNLOAD
+            </motion.button>
           </div>
-        </div>
+        </motion.div>
 
-          
-        <div className="home-right">
-          <img src={sammy} alt="myself"></img>
-        </div>
+        {/* Right Section */}
+        <motion.div
+          className="home-right"
+          initial={{ scale: 0.8, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+          <img src={sammy} alt="samuel kofi" />
+        </motion.div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
