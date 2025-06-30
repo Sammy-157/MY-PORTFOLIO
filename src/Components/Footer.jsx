7a6a6a2db8e88acf370b32cec8c22679b986cda1
Sammy-157 <samuelamankwah157@gmail.com>
2025-06-30 17:40:29 +0000

    footer update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,15 +5,17 @@ import '../Styles/Footer.css';
 
 const Footer = () => {
   return (
-    <div className='footer'>
-      <div className="image-icons">
-      <NavLink to="https://www.facebook.com/share/1AP8w2D8Mj/"><FaFacebook /></NavLink>
-        <NavLink to="https://www.instagram.com/digginssammy/"><FaInstagram /></NavLink>
-        <NavLink to="https://wa.me/233537387548"><FaWhatsapp /></NavLink> 
-        <NavLink to="https://www.linkedin.com/in/samuel-amankwah-785341323?"><FaLinkedin /></NavLink>
+    <footer className='footer'>
+      <div className="footer-content">
+        <div className="social-icons">
+          <NavLink to="https://www.facebook.com/share/1AP8w2D8Mj/" target='_blank'><FaFacebook /></NavLink>
+          <NavLink to="https://www.instagram.com/digginssammy/" target='_blank'><FaInstagram /></NavLink>
+          <NavLink to="https://wa.me/233537387548" target='_blank'><FaWhatsapp /></NavLink>
+          <NavLink to="https://www.linkedin.com/in/samuel-amankwah-785341323?" target='_blank'><FaLinkedin /></NavLink>
+        </div>
+        <p className="footer-text">© {new Date().getFullYear()} Samuel Amankwah. All rights reserved.</p>
       </div>
-      <b>Copyright ©{new Date().getFullYear()} All rights reserved</b>
-    </div>
+    </footer>
   );
 };
 
